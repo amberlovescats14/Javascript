bddc01cea5ba7d45ef80f5ee31e7cbbba4d3d459
Amber Jones <amberlovescats14@gmail.com>
2019-02-12 14:51:14 -0600

    I cant get math only adding

--- tip-calc/script.js
+++ tip-calc/script.js
@@ -2,15 +2,21 @@ const bill = document.querySelector("#bill");
 const tip = document.querySelector("#tip");
 const total = document.querySelector('.total-amount')
 const tipamount = document.querySelector('.tip-amount')
+
+
 function calculate() {
   console.log('hi');
   tipamount.innerHTML = bill.value * .15;
-  subtract()
 }
-function subtract() {
-  console.log('subtract')
-  total.innerHTML = (bill.value) - (tipamount.value)
+
+function addTotal() {
+  x = 1
+  if (bill.value > 1) {
+    x = bill.value * .15
+  }
+  console.log(x + bill.value)
 }
 
 bill.addEventListener('input', calculate )
+bill.addEventListener('input', addTotal)
 
